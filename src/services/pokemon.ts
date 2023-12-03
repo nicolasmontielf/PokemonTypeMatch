acab853aa20eeb867e0e57955012f12ff803a918
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-03 04:08:47 -0300

    Initial implementation of pokemon api

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,5 +1,16 @@
-import Pokedex from 'pokedex-promise-v2';
+// @ts-ignore
+import { Pokedex } from 'pokeapi-js-wrapper';
+import type { PokemonData } from '@/types/Pokemon';
+
 const P = new Pokedex({
     versionPath: '/api/v2/',
-    cacheLimit: 3600 * 1000, // 100s
+    cacheLimit: 3600 * 1000, // 1 hour
 });
+
+export async function getPokemon(name: string): Promise<PokemonData | undefined> {
+    try {
+        return await P.getPokemonByName(name);
+    } catch (_e) {
+        return undefined;
+    }
+}
