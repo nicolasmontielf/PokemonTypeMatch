981ee6b732e38ca13c30202e521204616cb422a5
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-03 23:59:17 -0300

    Fixed imported logo

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+    import logo from '@/assets/images/logo.png'
+</script>
+
 <template>
     <footer class="bg-white dark:bg-gray-900 shadow-inner">
         <div class="container flex flex-col items-center justify-between px-6 py-8 mx-auto lg:flex-row"> 
-            <img class="w-auto h-16" src="src/assets/images/logo.png" alt="Imagen del logo">
+            <img class="w-auto h-16" :src="logo" alt="Imagen del logo">
 
             <div class="flex flex-wrap items-center justify-center gap-4 mt-6 lg:gap-6 lg:mt-0">
                 
--- src/components/LayoutNavbar.vue
+++ src/components/LayoutNavbar.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+    import logo from '@/assets/images/logo.png'
+</script>
+
 <template>
     <nav class="relative bg-white shadow dark:bg-gray-800">
         <div class="container px-6 py-4 mx-auto flex justify-center">
             <div class="flex items-center justify-between">
-                <img class="w-auto h-12" src="src/assets/images/logo.png" alt="">
+                <img class="w-auto h-12" :src="logo" alt="">
             </div>
         </div>
     </nav>
