5693beafbc148347a578485fe0fc3375597c2e08
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-06 02:14:57 -0300

    Ability to add more than one enemy pokemon

--- src/components/enemy/EnemyTeam.vue
+++ src/components/enemy/EnemyTeam.vue
@@ -5,26 +5,34 @@
     import SearchPokemonInput from '@/components/common/SearchPokemonInput.vue';
     import { useCommonStore } from '@/stores/common';
 
-    const pokemon = ref<Pokemon>()
+    const pokemones = ref<Pokemon[]>([])
+    const selected = ref<number>();
 
     function finded(returned: Pokemon) {
-        pokemon.value = returned;
+        pokemones.value.push(returned)
+        selected.value = returned.id;
         useCommonStore().setEnemyTypes(returned.types);
     }
 
     function notFinded() {
-        pokemon.value = undefined;
+        // pokemon.value = undefined;
+        console.log('not finded')
     }
 
-    function removePokemon() {
-        pokemon.value = undefined;
+    function removePokemon(pokemonId: number) {
+        pokemones.value = pokemones.value.filter(p => p.id !== pokemonId)
+    }
+
+    function changeSelected(pokemon: Pokemon) {
+        selected.value = pokemon.id;
+        useCommonStore().setEnemyTypes(pokemon.types);
     }
 </script>
 
 <template>
     <div class="flex flex-col flex-wrap mt-2 mb-10">
         <div class="flex gap-3">
-            <h3 class="text-xl font-bold inline">Pokemon Enemigo</h3>
+            <h3 class="text-xl font-bold inline">Pokemones Enemigos</h3>
         </div>
 
         <div class="w-full">
@@ -34,12 +42,16 @@
             />
         </div>
 
-        <div class="w-full" v-if="pokemon">
-            <PokemonItem
-                :pokemon="pokemon"
-                @remove="removePokemon"
-                hide-features
-            />
+        <div class="w-full" v-if="pokemones.length">
+            <template v-for="pokemon of pokemones" :key="pokemon.id">
+                <PokemonItem
+                    :pokemon="pokemon"
+                    @remove="removePokemon"
+                    hide-features
+                    @click="changeSelected(pokemon)"
+                    :class="[selected === pokemon.id ? 'bg-gray-300' : '']"
+                />
+            </template>
         </div>
 
     </div>
